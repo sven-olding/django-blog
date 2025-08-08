10a1af974e157018d535a2106e33a1a54c8ebb9e
Sven Olding <sven.olding@vwgis.de>
2025-08-08 13:41:39 +0200

    add custom model manager for published posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,10 +2,18 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status = Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
         PUBLISHED = "PB", "Published"
+        
+    objects = models.Manager() # default
+    published = PublishedManager() # custom manager to fetch only published posts
         
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
