1f72f9ca78a740bb465d9c3e75841103f84addc0
Sven Olding <sven.olding@gmx.de>
2025-08-17 12:36:22 +0200

    add SEO friendly url to blog posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
 
@@ -17,7 +18,7 @@ class Post(models.Model):
     published = PublishedManager()  # custom manager to fetch only published posts
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -33,3 +34,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404, render
+
 from .models import Post
 
 
@@ -7,6 +8,13 @@ def post_list(request):
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        slug=post,
+    )
     return render(request, "blog/post/detail.html", {"post": post})
