0e54cd5f84617ed5ee0c7853c96feb3c083d0144
Sven Olding <sven.olding@vwgis.de>
2025-08-20 13:43:15 +0200

    add comment model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,6 +9,23 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
+class Comment(models.Model):
+    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ["created"]
+        indexes = [models.Index(fields=["created"])]
+
+    def __str__(self):
+        return f"Comment by {self.name} on {self.post}"
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = "DF", "Draft"
